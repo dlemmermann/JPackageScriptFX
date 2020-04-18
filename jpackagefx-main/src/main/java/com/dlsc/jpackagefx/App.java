738c6fb58179792a8043898e5f19d3d63444f722
mipastgt <mp@jugs.org>
2020-04-18 13:18:20 +0200

    Add dependency on JMetro
    Shows how dependecies on external libraries are handled transparently by
    the build process.

--- jpackagefx-main/src/main/java/com/dlsc/jpackagefx/App.java
+++ jpackagefx-main/src/main/java/com/dlsc/jpackagefx/App.java
@@ -17,8 +17,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.JMetroStyleClass;
+import jfxtras.styles.jmetro.Style;
 
 public class App extends Application {
+    
+    private final static boolean USE_JMETRO = true;
 
     @Override
     public void start(Stage primaryStage) {
@@ -29,6 +34,17 @@ public class App extends Application {
         box.setAlignment(Pos.CENTER);
         box.setPadding(new Insets(20));
         Scene scene = new Scene(box);
+        
+        if (USE_JMETRO) {
+            // This is just to show how dependencies to external libraries
+            // (in this case JMetro) are handled.
+            
+            JMetro jMetro = new JMetro(Style.DARK); // Or Style.LIGHT
+            jMetro.setScene(scene);
+            
+            box.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+        }
+        
         primaryStage.setScene(scene);
         primaryStage.setWidth(300);
         primaryStage.setHeight(300);
